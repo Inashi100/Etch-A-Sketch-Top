34adef748dc47b4103b3384204a00f344dd0ebd0
Inashi100 <Inashi100@users.noreply.github.com>
2023-08-04 02:26:16 +0100

    game functions completed

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
-document.addEventListener("DOMcontentLoaded", function(){
-createBoard(32);
-getSize();
-console.log("hi")
+let color = "black";
+let click = false;
+
+document.addEventListener("DOMContentLoaded", function(){
+   createBoard(16);
+   document.querySelector("body").addEventListener("click", function(e){
+    if(e.target.tagName != "BUTTON"){
+        click = !click;
+        let draw = document.querySelector("#draw");
+        if (click){
+            draw.innerHTML = "Now You can Draw";
+        }
+        else{
+            draw.innerHTML = "You're Not allowed To Draw" 
+        }
+    }
+   })
+   let btn_popup = document.querySelector("#popup");
+   btn_popup.addEventListener("click", function(){
+    let size = getsize();
+    createBoard(size);
+   })
 })
 
 function createBoard(size){
     let board = document.querySelector(".board");
-    board.style.gridTemplateColumns = 'repeat(${size}, 1fr)';
-    board.style.gridTemplateRows = 'repeat(${size}, 1fr)';
 
-    let numDivs = size * size;
-    for (let i = 0; i< numDivs; i++){
-        let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        board.insertAdjacentElement("beforeend", div);
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorsquare);
+    board.insertAdjacentElement("beforeend", square);
+}
+
+}
+function getsize(){
+    let input = prompt("what board size?");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number"
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Enter a number between 1 and 100"
+    }
+    else{
+        message.innerHTML = "Now you can play"
+        return input;
     }
 }
 
-function getSize(){
-    let input = prompt("Enter board size?")
+function colorsquare(){
+    if(click){
+    if(color == "Random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = 'black'
+    }
+}
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("divs")
+    divs.forEach((div) => div.style.backgroundcolor = white)
 }
